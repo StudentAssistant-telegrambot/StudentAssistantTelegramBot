0d10f796c68507b18ae5e97865489f3246d68fa6
Виталий <vitaliy98v@gmail.com>
2018-11-21 11:09:46 +0200

    Поменял сообщение и команду

--- src/main/java/MessageHandler.java
+++ src/main/java/MessageHandler.java
@@ -1,15 +1,15 @@
 class MessageHandler {
 
-    private String startAnswer = "Welcome to the Student assistant bot. Now it can answer to you. Hello world version";
+    private String startAnswer = "Darova ot Vitalika";
 
-    private String noAnswer = "I can't understand you. Hello!";
+    private String noAnswer = "Napiwi norm";
 
     MessageHandler(){
 
     }
 
     String textToText(String message){
-        if (message.toUpperCase().contentEquals("/START"))
+        if (message.toUpperCase().contentEquals("/STARTED"))
             return startAnswer;
 
 
