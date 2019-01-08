f6b8dd2624c0ac01161dab1ee8fb5c37e1672d0d
AndreyMokryj <andrey_mokryj@ukr.net>
2019-01-08 17:52:49 +0200

    добавлена команда /help

--- src/main/java/MessageHandler.java
+++ src/main/java/MessageHandler.java
@@ -6,6 +6,9 @@ class MessageHandler {
     private String startseccsessful = "Vasya Privet";
     private String noAnswer = "I can't understand you. Hello!";
 
+    private String helpAnswer = "/reg - зарегистрироваться\n/rteacher - зарегистрироваться преподавателем\n" +
+            "/lesson - текущая пара\n/today - расписание на сегодня\n";
+
 
 
     MessageHandler(){
@@ -26,6 +29,9 @@ class MessageHandler {
             return "Good morning!!!";
         }
 
+        if (message.toUpperCase().contentEquals("/HELP")) {
+            return helpAnswer;
+        }
 
 
         return noAnswer;
